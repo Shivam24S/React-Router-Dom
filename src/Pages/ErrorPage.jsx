e6bc8ffbe0e85f7dc1452cdb0cb231c4b5b150da
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-12 12:31:41 +0530

    useNavigate

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,12 +1,18 @@
-import Header from "../component/Header";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
   return (
     <>
-      <Header />
       <main>
         <h1>An error occurred</h1>
         <p>this Page is not available</p>
+        <button onClick={handleNavigate}>Home</button>
       </main>
     </>
   );
